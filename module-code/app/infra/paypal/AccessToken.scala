3b3286906cec465a3fc7ee444b4673ff2ec1f676
Dmitry Kurinskiy <name.alari@gmail.com>
2014-10-16 18:10:40 +0400

    access token is also a codomain

--- module-code/app/infra/paypal/AccessToken.scala
+++ module-code/app/infra/paypal/AccessToken.scala
@@ -6,7 +6,7 @@ import play.api.libs.json.Json
  * @author alari
  * @since 7/24/14
  */
-case class AccessToken(scope: String, access_token: String, token_type: String, app_id: String, expires_in: Int) extends Serializable
+case class AccessToken(scope: String, access_token: String, token_type: String, app_id: String, expires_in: Int) extends Serializable with Codomain
 
 object AccessToken {
   implicit val format = Json.format[AccessToken]
